803bbadf4bd752e86c450b431826ac94d74b4bef
Emilaine Borato <emilaineborato@gmail.com>
2022-08-18 13:00:07 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Aifos'
+copyright = '2022, Emilaine'
+author = 'Emilaine'
 
 release = '0.1'
 version = '0.1.0'
